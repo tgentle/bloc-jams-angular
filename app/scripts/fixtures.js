f859a0e2c035b1ad8d353a7fa58f24d43cf5f607
tgentle <taylormgentle@gmail.com>
2017-05-02 19:37:16 -0400

    angular-5-controller complete

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,47 @@
+//Example Album
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
+        { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
+        { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
+        { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink' },
+        { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta' }  
+    ]
+};
+
+//Another Example Album
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+//My Album Object
+var albumGaga = {
+    title: 'ArtPOP',
+    artist: 'Lady Gaga',
+    label: 'Insomniac/Interscope Records',
+    year: '2013',
+    albumArtUrl: 'assets/images/album_covers/artpop_cover.png',
+    songs: [
+        { title: 'Aura', duration: '3:55' },
+        { title: 'Venus', duration: '3:53' },
+        { title: 'G.U.Y.', duration: '3:52'},
+        { title: 'MANiCURE', duration: '3:19' },
+        { title: 'Donatella', duration: '4:24'}
+    ]
+};

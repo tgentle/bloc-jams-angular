c50bb794db8e0d28e4f4c12f0cc1a212fe053c0f
tgentle <taylormgentle@gmail.com>
2017-05-15 21:29:25 -0400

    assignment-angular-7-services2 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -20,26 +20,43 @@ function SongPlayer() {
       currentSong.playing = null;
     }
 
-  currentBuzzObject = new buzz.sound(song.audioUrl, {
+    currentBuzzObject = new buzz.sound(song.audioUrl, {
       formats: ['mp3'],
       preload: true
     });
 
     currentSong = song;
   };
-
-  SongPlayer.play = function(song) {
-    setSong(song);
+  /**
+  * @function playSong
+  * @desc Plays currentBuzzObject and sets the property of the song object to true
+  * @param {Object} song
+  */
+  var playSong = function(song){
     currentBuzzObject.play();
     song.playing = true;
+  }
+  /**
+  * @function SongPlayer.play
+  * @desc If buzz object song is not the same as current then a new song
+  * will load and play. If the buzz object song is the same, and if the song is paused, then it will play
+  * @param {Object} song
+  */
+  SongPlayer.play = function(song) {
+    setSong(song);
+    playSong(song);
   } else if (currentSong == song) {
     if (currentBuzzObject.isPaused()) {
-      currentBuzzObject.play();
+      play(song);
     }
   }
 };
 
-
+/**
+* @function SongPlayer.pause
+* @desc Pauses the currently playing buzz object and sets song's playing attribute to false
+* @param {Object} song
+*/
 SongPlayer.pause = function(song) {
   currentBuzzObject.pause();
   song.playing = false;
